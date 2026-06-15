195eade654df83bab6374d139c0d7cc0e6a5dfe4
devkrish07 <skreddy.dev@gmail.com>
2026-06-15 19:05:05 +0530

    web token updated

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -35,9 +35,8 @@ import appleAuth, {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 GoogleSignin.configure({
-  scopes: ['https://www.googleapis.com/auth/drive.readonly'],
   webClientId:
-    '396564745764-disnuci9msclu3j7i3r9knke7b9qtr9f.apps.googleusercontent.com',
+    '396564745764-lk21f8ddr1nshcp3gsbqtkvjj692e5tt.apps.googleusercontent.com',
   iosClientId:
     '396564745764-pg61tt1q905j1sol45agoj7kb1htc28a.apps.googleusercontent.com',
 });
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -38,9 +38,8 @@ import CustomSafeAreaView from '@components/CustomSafeAreaView';
 
 
 GoogleSignin.configure({
-  scopes: ['https://www.googleapis.com/auth/drive.readonly'],
   webClientId:
-    '396564745764-disnuci9msclu3j7i3r9knke7b9qtr9f.apps.googleusercontent.com',
+    '396564745764-lk21f8ddr1nshcp3gsbqtkvjj692e5tt.apps.googleusercontent.com',
   iosClientId:
     '396564745764-pg61tt1q905j1sol45agoj7kb1htc28a.apps.googleusercontent.com',
 });

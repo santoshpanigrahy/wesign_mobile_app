94107e67bb6350032c0869a4c2416858d78e867b
Vipul Singhal <singhalvipul53@gmail.com>
2026-06-25 13:36:26 +0530

    Clarity addition

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import * as Clarity from '@microsoft/react-native-clarity';
+
+Clarity.initialize('xc2p8g5zzf', {
+  logLevel: Clarity.LogLevel.None,
+});
+
 AppRegistry.registerComponent(appName, () => App);
